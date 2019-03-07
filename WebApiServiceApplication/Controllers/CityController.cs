78cf57aa55250eab415d8261b1bd4066b3846a28
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2019-03-07 11:26:58 +0300

    WepApiServiceApplication

---
+++ WebApiServiceApplication/Controllers/CityController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiServiceApplication.Controllers
+{
+    public class CityController : ApiController
+    {
+        private string[] sehirler = new string[] { "Ankara", "İstanbul", "Çorum" }; //string türünden array döndürelim
+
+        public string[] Get() //Eğer Parametre gönderilmemişse dizinin hepsini alır.
+        {
+
+            //return ("Merhaba Wep Api");
+            return sehirler;
+ 
+        }
+        public string Get(int id) //parametre gönderildiği için string tipinde dizi içerisinde parametreye ait indis e karşılık gelen değeri gösterir.
+        {
+            return sehirler[id];
+        }
+
+    }
+}
